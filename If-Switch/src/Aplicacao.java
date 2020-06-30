0231e4ac7b847717f9032e77c10c727f216089e9
Edson Junior <edson_junior91@hotmail.com>
2020-06-30 00:44:25 -0300

    Se e Switch

---
+++ If-Switch/src/Aplicacao.java
@@ -0,0 +1,37 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+//		int num = 9;
+//		
+//		if (num > 10) { 
+//			System.out.println("O número é maior que 10");
+//		}else if( num < 10) { 
+//			System.out.println("O número é menor que 10");
+//		}else {
+//			System.out.println("O número é 10");
+//			System.out.println();
+//		}
+		
+		int num = 15;
+		
+		switch (num) {
+		case 5:
+			System.out.println("O Número é 5");
+			break;
+		case 10:
+			System.out.println("o Número é 10");
+			break;
+		case 15:
+			System.out.println("o Número é 15");
+			break;
+		default:
+			System.out.println("O número é outro");
+			break;
+		}
+	
+	
+	}
+
+}
